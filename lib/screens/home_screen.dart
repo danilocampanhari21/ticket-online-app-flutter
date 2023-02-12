27a0121771808eb0aac7cf4f37c3153e40f43573
Danilo Campanhari <danilocampanhari64@gmail.com>
2023-02-12 18:56:03 +0100

    updanting ...

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,15 +11,36 @@ class HomeScreen extends StatelessWidget {
       body: ListView(
         children: [
           Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
             child: Column(
               children: [
                 Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text(
-                      "Good Morning"
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          "Good Morning"
+                        ),
+                        Text(
+                          "Book Tickets"
+                        ),
+                      ],
                     ),
-                    Image.asset("/assets/images/img_1.png")
-
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                          fit: BoxFit.fitHeight,
+                          image: AssetImage(
+                            "assets/images/img_1.png"
+                          )
+                        )
+                      ),
+                    )
                   ],
                 )
               ],
