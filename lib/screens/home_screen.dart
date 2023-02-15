97d77453eb3cf6fce368b08cfc91e8e43777a803
Danilo Campanhari <danilocampanhari64@gmail.com>
2023-02-15 10:08:48 +0100

    updanting ...

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:untitled1/screens/ticket_view.dart';
 
 import '../utils/app_styles.dart';
 
@@ -65,10 +66,24 @@ class HomeScreen extends StatelessWidget {
                       ),
                     ],
                   ),
+                ),
+                const Gap(40),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Upcoming Flights", style: Styles.headLineStyle2,),
+                    InkWell(
+                      onTap: (){
+                        print("You are tapped");
+                      },
+                        child: Text("View all", style: Styles.textStyle.copyWith(color: Styles.primaryColor),))
+                  ],
                 )
               ],
             ),
-          )
+          ),
+          const Gap(15),
+          TicketView(),
         ],
       ),
     );
